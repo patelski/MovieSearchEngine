64fbf37768ff4393fa53ed459da05b768cef74e7
Stefan Patelski <github@patelski.eu>
2013-09-26 17:17:26 +0200

    Created main source folder

---
+++ MovieSearchEngine/src/moviesearchengine/MovieSearchEngine.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moviesearchengine;
+
+/**
+ *
+ * @author Stefan
+ */
+public class MovieSearchEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
